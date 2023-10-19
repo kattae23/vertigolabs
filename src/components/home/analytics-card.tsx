6eb99717e92d29ba8059b86e59f12dc09292c0ea
kattae23 <kattae.kattae@gmail.com>
2023-10-19 16:46:57 -0400

    section added

---
+++ src/components/home/analytics-card.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { IconType } from 'react-icons'
+
+type Props = {
+  Icon: IconType
+  className: string
+  fetchText: string | number
+  label: string
+};
+
+const AnalyticsCard = ({ Icon, className, fetchText, label }: Props) => {
+  return (
+    <div className='flex w-full lg:w-2/4 min-h-[131px] max-h-[131px] flex-row border border-r-0 border-gray-400/50 rounded-md rounded-r-none'>
+      <div className='flex w-full flex-row justify-center items-center py-9'>
+        <div className='w-1/4 px-10'>
+          <Icon className={className} />
+        </div>
+        <div className='w-3/4 px-4'>
+          <div className='flex flex-col text-center items-center justify-center'>
+            <h2 className='font-bold text-lg text-gray-800/90'>{fetchText}</h2>
+            <p className='text-gray-500/90 text-sm font-normal'>{label}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AnalyticsCard
--- src/components/home/analytics.tsx
+++ src/components/home/analytics.tsx
@@ -1,135 +1,115 @@
 import { AnalyticsArrType } from '@/app/api/analytics/route'
 import React from 'react'
-import { FaBriefcase, FaHandshake, FaRegMoneyBillAlt } from 'react-icons/fa'
+import { FaBriefcase, FaHandshake, FaPercent, FaRegMoneyBillAlt } from 'react-icons/fa'
 import { TbReload } from 'react-icons/tb'
+import { IoMdAnalytics } from 'react-icons/io'
+import AnalyticsCard from './analytics-card'
+import { BsFillCalendarMonthFill } from 'react-icons/bs'
+import { ImClock } from 'react-icons/im'
+import { AiOutlineInfoCircle } from 'react-icons/ai'
+import { HiRocketLaunch } from 'react-icons/hi2'
 
 const fetchAnalytics = () => {
-  return fetch(process.env.NEXTAPIURL! + '/api/analytics', {
-    next: {
-      revalidate: 60
-    }
-  }).then(res => res.json()) as Promise<AnalyticsArrType>
+  return fetch(process.env.NEXTAUTH_URL! + '/api/analytics')
+    .then(res => res.json()) as Promise<AnalyticsArrType>
 }
 
 const Analytics = async () => {
   const resp = await fetchAnalytics()
-
   return (
-    <section className='w-full h-[80vh] bg-[#EEEEEE] flex justify-center px-5 md:px-32 flex-col'>
+    <section className='w-full h-auto bg-[#EEEEEE] flex justify-center items-center px-5 md:px-32 flex-col'>
       <h2 className='text-3xl text-gray-800 font-light mb-16 text-center'>Los proyectos con garantía hipotecaria <span className='font-bold'>en cifras</span></h2>
-      <div className='max-w-fit gap-7 flex flex-col md:flex-row'>
-        <div className='max-w-fit bg-white flex flex-col'>
-          <div className='w-full flex flex-row'>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-r-0 border-gray-400/50 rounded-md rounded-r-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <FaHandshake className='text-blue-200 text-5xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>{resp.financedProjects}</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Proyectos financiados</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-gray-400/50 rounded-md rounded-l-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <FaBriefcase className='text-orange-500/80 text-4xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>€ {resp.investmentRaised} M</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Proyectos financiados</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+      <div className='w-full xl:max-w-fit gap-7 flex flex-col min-[1200px]:flex-row justify-center items-center'>
+        <div className='w-full bg-white flex flex-col'>
+          <div className='w-full flex flex-col md:flex-row'>
+            <AnalyticsCard
+              Icon={FaHandshake}
+              className='text-blue-200 text-5xl'
+              fetchText={resp.financedProjects}
+              label='Proyectos financiados'
+            />
+            <AnalyticsCard
+              Icon={FaBriefcase}
+              className='text-orange-500/80 text-4xl'
+              fetchText={'$ ' + resp.investmentRaised + ' M'}
+              label='Inversión captada'
+            />
           </div>
-          <div className='w-full flex flex-row'>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-r-0 border-gray-400/50 rounded-md rounded-r-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <TbReload className='text-lime-500 transform scale-x-[-1] text-5xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>{resp.returnedProjects}</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Proyectos financiados</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-gray-400/50 rounded-md rounded-l-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <FaRegMoneyBillAlt className='text-blue-600 text-4xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>€ {resp.returnsAndDeliveries} M</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Proyectos financiados</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div className='w-full flex flex-col md:flex-row'>
+            <AnalyticsCard
+              Icon={TbReload}
+              className='text-lime-500 transform scale-x-[-1] text-5xl'
+              fetchText={resp.returnedProjects}
+              label='Proyectos devueltos'
+            />
+            <AnalyticsCard
+              Icon={FaRegMoneyBillAlt}
+              className='text-blue-600 text-4xl'
+              fetchText={resp.returnsAndDeliveries}
+              label='Devoluciones y repartos'
+            />
           </div>
         </div>
-        <div className='max-w-fit bg-white flex flex-col'>
-          <div className='w-full flex flex-row'>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-r-0 border-gray-400/50 rounded-md rounded-r-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <FaHandshake className='text-blue-200 text-5xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>{resp.averageProjectAmount} $</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Importe medio proyecto</p>
-                  </div>
-                </div>
+        <div className='w-full bg-white flex flex-col'>
+          <div className='w-full flex flex-col md:flex-row'>
+            <AnalyticsCard
+              Icon={IoMdAnalytics}
+              className='text-[#FD7B61] text-4xl'
+              fetchText={resp.averageProjectAmount}
+              label='Importe medio proyecto'
+            />
+            <AnalyticsCard
+              Icon={FaPercent}
+              className='text-[#8FCCC9] text-4xl'
+              fetchText={resp.averageLtv + '%'}
+              label='LTV medio'
+            />
+          </div>
+          <div className='w-full flex flex-col md:flex-row'>
+            <AnalyticsCard
+              Icon={BsFillCalendarMonthFill}
+              className='text-[#80BE15] text-4xl'
+              fetchText={resp.averageDurationExpectedProject}
+              label='Duración media prevista proyecto'
+            />
+            <AnalyticsCard
+              Icon={ImClock}
+              className='text-[#1A71C1] text-4xl scale-x-[-1]'
+              fetchText={resp.averageDurationEndOfProject}
+              label='Duración media final proyecto'
+            />
+          </div>
+        </div>
+      </div>
+      <div className='w-full bg-white flex flex-col mt-10 rounded-md'>
+        <div className='w-full flex flex-col xl:flex-row'>
+          <div className='flex w-full xl:w-2/4 min-h-[131px] max-h-[131px] flex-row border border-r-0 border-gray-400/50 rounded-md rounded-r-none text-[#333333]'>
+            <div className='flex w-full h-full flex-row justify-center items-center'>
+              <div className='w-1/4 px-14 bg-[#6BCCA4] h-full flex justify-center items-center'>
+                <HiRocketLaunch className='text-3xl' />
               </div>
-            </div>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-gray-400/50 rounded-md rounded-l-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <FaBriefcase className='text-orange-500/80 text-4xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>€ {resp.averageLtv} M</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Proyectos financiados</p>
+              <div className='w-3/4 px-4 bg-[#B5E5D1] h-full flex items-center justify-center'>
+                <div className='flex flex-col text-center items-center justify-center'>
+                  <h2 className='font-bold text-3xl'>8,83%</h2>
+                  <div className='flex flec-row'>
+                    <p className='font-normal'>TIR media conseguida</p>
+                    <AiOutlineInfoCircle className='ml-1' />
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className='w-full flex flex-row'>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-r-0 border-gray-400/50 rounded-md rounded-r-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <TbReload className='text-lime-500 transform scale-x-[-1] text-5xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>{resp.averageDurationExpectedProject}</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Duración media <br />
-                      prevista proyecto
-                    </p>
-                  </div>
-                </div>
+          <div className='flex w-full xl:w-2/4 min-h-[131px] max-h-[131px] flex-row border border-r-0 border-gray-400/50 rounded-md rounded-r-none text-[#333333]'>
+            <div className='flex w-full h-full flex-row justify-center items-center'>
+              <div className='w-1/4 px-14 bg-[#6BCCA4] h-full flex justify-center items-center'>
+                <HiRocketLaunch className='text-3xl' />
               </div>
-            </div>
-            <div className='flex w-2/4 min-h-[131px] max-h-[131px] flex-row border border-gray-400/50 rounded-md rounded-l-none'>
-              <div className='flex w-full flex-row justify-center items-center py-9'>
-                <div className='w-1/4 px-10'>
-                  <FaRegMoneyBillAlt className='text-blue-600 text-4xl' />
-                </div>
-                <div className='w-3/4 px-4'>
-                  <div className='flex flex-col text-center items-center justify-center'>
-                    <h2 className='font-bold text-lg text-gray-800/90'>{resp.averageDurationEndOfProject}</h2>
-                    <p className='text-gray-500/90 text-sm font-normal'>Proyectos financiados</p>
+              <div className='w-3/4 px-4 bg-[#B5E5D1] h-full flex items-center justify-center'>
+                <div className='flex flex-col text-center items-center justify-center'>
+                  <h2 className='font-bold text-3xl'>8,83%</h2>
+                  <div className='flex flec-row'>
+                    <p className='font-normal'>TIR media conseguida</p>
+                    <AiOutlineInfoCircle className='ml-1' />
                   </div>
                 </div>
               </div>
