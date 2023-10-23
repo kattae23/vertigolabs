4f9e44c9f82ed2f742150a7776930cab000c6e69
kattae23 <kattae.kattae@gmail.com>
2023-10-23 02:08:36 -0400

    updates login and other things

--- src/components/auth/user-auth-form.tsx
+++ src/components/auth/user-auth-form.tsx
@@ -130,6 +130,16 @@ export function UserAuthForm ({ className, ...props }: UserAuthFormProps) {
             <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
             )
           : (
+            <Icons.google className='mr-2 h-4 w-4' />
+            )}{' '}
+        Google
+      </Button>
+      <Button variant='outline' type='button' disabled={isLoading}>
+        {isLoading
+          ? (
+            <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
+            )
+          : (
             <Icons.gitHub className='mr-2 h-4 w-4' />
             )}{' '}
         Github
