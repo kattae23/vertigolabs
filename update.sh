ba1c31d8fe48e299471f13c58719e7c35d5fd5b4
kattae23 <kattae.kattae@gmail.com>
2023-10-24 15:32:05 -0400

    test 15 workflow githubactions

--- update.sh
+++ update.sh
@@ -9,11 +9,11 @@ executeCommands() {
 
     git pull origin dev || { echo "Failed to execute git pull."; exit 1; }
  
-    npm install || { echo "Failed to execute pnpm install."; exit 1; }
+    pnpm install || { echo "Failed to execute pnpm install."; exit 1; }
  
-    npm run build || { echo "Failed to execute pnpm build."; exit 1; }
+    pnpm build || { echo "Failed to execute pnpm build."; exit 1; }
  
-    pm2 restart vertigolabs || { echo "Failed to execute pm2 restart."; exit 1; }
+    pm2 start pnpm --name vertigolabs -- run start -- -p 3000
 }
  
 executeCommands "./"
