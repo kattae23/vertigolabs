8008e7d9a909774dc0567db1aec0aeda32e44322
kattae23 <kattae.kattae@gmail.com>
2023-10-24 15:18:59 -0400

    test 12 workflow githubactions

--- update.sh
+++ update.sh
@@ -5,11 +5,13 @@ executeCommands() {
  
     cd "$directory" || { echo "Directory not found."; exit 1; }
  
-    git pull || { echo "Failed to execute git pull."; exit 1; }
+    git fetch origin dev || { echo "Failed to execute git fetch."; exit 1; }
+
+    git pull origin dev || { echo "Failed to execute git pull."; exit 1; }
  
-    pnpm install || { echo "Failed to execute pnpm install."; exit 1; }
+    npm install || { echo "Failed to execute pnpm install."; exit 1; }
  
-    pnpm build || { echo "Failed to execute pnpm build."; exit 1; }
+    npm build || { echo "Failed to execute pnpm build."; exit 1; }
  
     pm2 restart project || { echo "Failed to execute pm2 restart."; exit 1; }
 }
