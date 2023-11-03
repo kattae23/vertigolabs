c33fcfcdb38b2c405ad502bc20976e52e6711212
kattae23 <kattae.kattae@gmail.com>
2023-11-02 22:15:32 -0400

    7cVTc7rLffNdlNiy

--- src/app/(links)/estadisticas/page.tsx
+++ src/app/(links)/estadisticas/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 const StadisticsPage = async () => {
   const analytics = await fetchAnalytics()
   return (
-    <main className='mb-10 mt-[96px]'>
+    <main className='mb-10 mt-[77px]'>
       <section className='w-full h-auto bg-[#EEEEEE] flex justify-center items-center px-5 md:px-32 flex-col text-[#493d3e]'>
         <h2 className='text-3xl font-bold mb-3 mt-16 text-center'>Los valores no son solo números</h2>
         <h2 className='text-3xl font-light mb-16 text-center'>Los proyectos con garantía hipotecaria <span className='font-bold'>en cifras</span></h2>
--- src/components/sign-in-button.tsx
+++ src/components/sign-in-button.tsx
@@ -6,11 +6,12 @@ interface Props {
     label: string;
     value: string;
     fix: boolean;
+    fixWork: boolean;
 }
 
-const SignInButton = ({ value, label, fix }: Props) => {
+const SignInButton = ({ value, label, fix, fixWork }: Props) => {
   return (
-    <Link href={process.env.NEXT_PUBLIC_URL + value} className={clsx('hidden md:inline-block font-medium text-sm ml-5 hover:underline', fix ? 'text-[#483c3d]' : 'text-white')}>
+    <Link href={process.env.NEXT_PUBLIC_URL + value} className={clsx('hidden md:inline-block font-medium text-sm ml-5 hover:underline', fixWork ? fix ? 'text-[#483c3d]' : 'text-white' : 'text-[#483c3d]')}>
       {label.toUpperCase()}
     </Link>
   )
