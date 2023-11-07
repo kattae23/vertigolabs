99ce688a25348dc73212b61dd0d9869836e42c33
kattae23 <kattae.kattae@gmail.com>
2023-11-07 19:07:46 -0400

    Oci/DccnrZpevum0

--- prisma/migrations/20231104173220_pipe/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `accountState` ENUM('premium', 'uncompleted') NOT NULL DEFAULT 'uncompleted';
--- prisma/migrations/20231104224236_pipeee/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `money` DOUBLE NOT NULL DEFAULT 0.00;
